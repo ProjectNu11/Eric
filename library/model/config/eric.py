edbe834fc12863e18e2d9aeae8e7485303491c62
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-10-13 22:08:15 +0800

    :construction: Update EricConfig

--- library/model/config/eric.py
+++ library/model/config/eric.py
@@ -1,5 +1,11 @@
+from abc import ABC
+from datetime import datetime
+from json import JSONDecodeError
 from pathlib import Path
+from typing import Type
 
+from creart import AbstractCreator, CreateTargetInfo, add_creator, exists_module
+from loguru import logger
 from pydantic import AnyHttpUrl, BaseModel, validator
 from typing_extensions import Self
 
@@ -86,3 +92,38 @@ class EricConfig(BaseModel):
         if value in {"pip", "poetry"}:
             return value
         raise ValueError("仅支持 pip 和 poetry 两种包管理器")
+
+
+class EricConfigClassCreator(AbstractCreator, ABC):
+    targets = (CreateTargetInfo("library.model.config.eric", "EricConfig"),)
+
+    @staticmethod
+    def available() -> bool:
+        return exists_module("library.model.config.eric")
+
+    @staticmethod
+    def create(create_type: Type[EricConfig]) -> EricConfig:
+        try:
+            config = EricConfig().from_file()
+        except FileNotFoundError:
+            config = EricConfig()
+            config.to_file()
+            logger.warning("配置文件不存在，已创建默认配置文件")
+            logger.warning("请修改配置文件后重启")
+            exit(1)
+        except JSONDecodeError:
+            CONFIG_FILE_PATH.rename(
+                CONFIG_FILE_PATH.with_suffix(
+                    f".json.bak{datetime.now().strftime('%Y%m%d%H%M%S')}"
+                )
+            )
+            config = EricConfig()
+            config.to_file()
+            logger.error("配置文件解析错误，请检查配置文件")
+            logger.warning("已备份原配置文件并创建默认配置文件")
+            exit(1)
+
+        return config
+
+
+add_creator(EricConfigClassCreator)
