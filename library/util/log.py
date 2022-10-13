073cb820621f3ca3409ede6f8fa12355a06190e1
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-10-14 01:09:05 +0800

    :construction: Setup logger

---
+++ library/util/log.py
@@ -0,0 +1,26 @@
+from datetime import time
+from pathlib import Path
+
+from creart import create
+from loguru import logger
+
+from library.model.config.eric import EricConfig
+
+
+def setup_logger():
+    config = create(EricConfig)
+    log_dir = config.path.log
+    logger.add(
+        Path(log_dir, "{time:YYYY-MM-DD}", "common.log"),
+        level="INFO",
+        retention=f"{config.log_rotate} days" if config.log_rotate else None,
+        encoding="utf-8",
+        rotation=time(),
+    )
+    logger.add(
+        Path(log_dir, "{time:YYYY-MM-DD}", "error.log"),
+        level="ERROR",
+        retention=f"{config.log_rotate} days" if config.log_rotate else None,
+        encoding="utf-8",
+        rotation=time(),
+    )
