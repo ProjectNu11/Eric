28e665adf69981e18e37bf776b1fe1dab3176a5d
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-10-16 22:45:55 +0800

    :construction: Update `FastAPIService`

--- library/service/fastapi.py
+++ library/service/fastapi.py
@@ -1,8 +1,30 @@
+from abc import ABC
+from typing import Type
+
+from creart import AbstractCreator, CreateTargetInfo, exists_module, create, add_creator
 from fastapi import FastAPI
 from graia.amnesia.builtins.starlette import StarletteService
+
+from library.model.config.eric import EricConfig
+
+
+class FastAPICreator(AbstractCreator, ABC):
+    targets = (CreateTargetInfo("fastapi.applications", "FastAPI"),)
+
+    @staticmethod
+    def available() -> bool:
+        return exists_module("fastapi.applications")
+
+    @staticmethod
+    def create(create_type: Type[FastAPI]) -> FastAPI:
+        config = create(EricConfig)
+        return FastAPI(**config.service.fastapi.params)
+
+
+add_creator(FastAPICreator)
 
 
 class FastAPIService(StarletteService):
     def __init__(self, fastapi: FastAPI | None = None) -> None:
-        self.fastapi = fastapi or FastAPI()
+        self.fastapi = fastapi or create(FastAPI)
         super().__init__(self.fastapi)
