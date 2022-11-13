5e01dcb2412dd7fadee681774d312f8546358484
nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>
2022-11-13 11:29:35 +0800

    :sparkles: Add `exception_handler`
    Signed-off-by: nullqwertyuiop <49110142+nullqwertyuiop@users.noreply.github.com>

--- library/decorator/permission.py
+++ library/decorator/permission.py
@@ -13,17 +13,18 @@ from library.util.message import send_message
 class Permission:
     @classmethod
     def require(
-        cls, level: UserPerm, on_failure: str = "权限不足，你需要来自 {permission} 的权限"
+        cls, level: UserPerm, on_failure: str | None = "权限不足，你需要来自 {permission} 的权限"
     ) -> Depend:
         async def check(app: Ariadne, event: MessageEvent) -> NoReturn:
             if await UserPerm.get(event.sender) < level:
-                await send_message(
-                    event.sender.group
-                    if isinstance(event, GroupMessage)
-                    else event.sender,
-                    MessageChain(on_failure.format(permission=level.value[-1])),
-                    app.account,
-                )
+                if on_failure:
+                    await send_message(
+                        event.sender.group
+                        if isinstance(event, GroupMessage)
+                        else event.sender,
+                        MessageChain(on_failure.format(permission=level.value[-1])),
+                        app.account,
+                    )
                 raise ExecutionStop()
             return
 
