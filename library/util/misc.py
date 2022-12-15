8f107d0e87f5752df6a19b02e89630c408a8595f
nullqwertyuiop <null@member.fsf.org>
2022-12-15 17:33:12 +0800

    :sparkles: Refine utils
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/util/misc.py
+++ library/util/misc.py
@@ -1,19 +1,66 @@
 import re
-from typing import Generator
+from typing import Generator, Iterable, TypeVar
+
+_T = TypeVar("_T")
 
 
-def seconds_to_string(seconds: int) -> str:
-    minutes, seconds = divmod(seconds, 60)
+def seconds_to_string(
+    time: int | float,
+    *,
+    repr_ms: bool = True,
+    ms_accuracy: int = 2,
+    zh_cn: bool = True,
+) -> str:
+    """
+    将秒数转换为字符串
+
+    Args:
+        time: 秒数
+        repr_ms: 是否显示毫秒
+        ms_accuracy: 毫秒精度
+        zh_cn: 是否使用中文
+
+    Returns:
+        转换后的字符串
+    """
+    seconds, ms = divmod(time, 1)
+    minutes, seconds = divmod(time, 60)
     hours, minutes = divmod(minutes, 60)
-    interval_repr = f"{seconds:02d} 秒"
-    if minutes:
-        interval_repr = f"{minutes:02d} 分 {interval_repr}"
-    if hours:
-        interval_repr = f"{hours:02d} 时 {interval_repr}"
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    months, weeks = divmod(weeks, 4)
+    years, months = divmod(months, 12)
+    seconds = int(seconds)
+    interval_repr = f"{seconds:02d}{' 秒' if zh_cn else 's'}"
+    if ms and repr_ms:
+        interval_repr += (
+            f" {str(int(ms * 1000))[:ms_accuracy]}{' 毫秒' if zh_cn else 'ms'}"
+        )
+    if minutes := int(minutes):
+        interval_repr = f"{minutes:02d}{' 分' if zh_cn else 'm'} {interval_repr}"
+    if hours := int(hours):
+        interval_repr = f"{hours:02d}{' 时' if zh_cn else 'h'} {interval_repr}"
+    if days := int(days):
+        interval_repr = f"{days}{' 天' if zh_cn else 'd'} {interval_repr}"
+    if weeks := int(weeks):
+        interval_repr = f"{weeks}{' 周' if zh_cn else 'w'} {interval_repr}"
+    if months := int(months):
+        interval_repr = f"{months}{' 月' if zh_cn else 'M'} {interval_repr}"
+    if years := int(years):
+        interval_repr = f"{years}{' 年' if zh_cn else 'y'} {interval_repr}"
     return interval_repr
 
 
-def inflate(*obj) -> list:
+def inflate(*obj: Iterable[_T]) -> list[_T]:
+    """
+    将多个可迭代对象合并为一个列表
+
+    Args:
+        obj: 可迭代对象
+
+    Returns:
+        合并后的列表
+    """
     result = []
     for o in obj:
         if isinstance(o, Generator):
