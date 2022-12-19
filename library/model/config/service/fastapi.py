3d95cf0e59a5f817a3846c748485c236574bee7e
nullqwertyuiop <null@member.fsf.org>
2022-12-19 21:36:09 +0800

    :construction: `FastAPIConfig`: Removed unnecessary properties
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/model/config/service/fastapi.py
+++ library/model/config/service/fastapi.py
@@ -27,19 +27,9 @@ class FastAPIConfig:
     """ FastAPI 服务器参数，将在启动时传入 """
 
     @property
-    def is_exposed(self) -> bool:
-        """是否已暴露"""
-        return self.domain or self.host != "127.0.0.1"
-
-    @property
     def link(self) -> str:
         """FastAPI 服务器链接"""
         protocol = "https" if self.https else "http"
         address = self.domain or self.host
         port = f":{self.port}" if self.show_port else ""
         return f"{protocol}://{address}{port}"
-
-    @property
-    def local_link(self) -> str:
-        """FastAPI 本地服务器链接，包含端口"""
-        return f"{self.host}:{self.port}"
