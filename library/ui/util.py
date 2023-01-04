147be34495c3ebe31dce242df3e5dcd9072e2957
nullqwertyuiop <null@member.fsf.org>
2023-01-04 16:13:38 +0800

    :bug: `library.ui`: Fixed typing

--- library/ui/util.py
+++ library/ui/util.py
@@ -2,6 +2,9 @@ import re
 from typing import TypeVar
 
 from lxml.builder import E
+from lxml.etree import _Element  # noqa
+
+# type(E.DIV()) == lxml.etree._Element
 
 HYPERLINK_PATTERN = re.compile(r"https?://\S+")
 _T = TypeVar("_T")
@@ -9,7 +12,7 @@ _T = TypeVar("_T")
 
 def wrap_text(
     text: str | list, newline: bool = True, hyperlink: bool = True
-) -> list[str | E]:
+) -> list[str | _Element]:
     wrapped = [text] if isinstance(text, str) else text
     if hyperlink:
         wrapped = _add_hyperlink(wrapped)
@@ -18,7 +21,7 @@ def wrap_text(
     return wrapped
 
 
-def _newline_to_br(text: list[_T]) -> list[_T | E]:
+def _newline_to_br(text: list[_T]) -> list[_T | _Element]:
     wrapped = []
     for part in text:
         if not isinstance(part, str):
@@ -31,14 +34,14 @@ def _newline_to_br(text: list[_T]) -> list[_T | E]:
     return wrapped
 
 
-def _replace_with_hyperlink(text: str, link: str) -> list[str | E]:
+def _replace_with_hyperlink(text: str, link: str) -> list[str | _Element]:
     wrapped = []
     for piece in text.split(link):
         wrapped.extend((piece, E.a(link, href=link)))
     return wrapped[:-1]
 
 
-def _add_hyperlink(text: list[_T]) -> list[_T | E]:
+def _add_hyperlink(text: list[_T]) -> list[_T | _Element]:
     # wrapped = []
     # for part in text:
     #     if not isinstance(part, str):
