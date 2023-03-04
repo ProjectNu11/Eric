0d80fcb76427c053332cde48302a07694c6560ff
nullqwertyuiop <null@member.fsf.org>
2023-03-04 22:17:17 +0800

    :pencil: Better version
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/__init__.py
+++ library/__init__.py
@@ -1,4 +1,26 @@
 from datetime import datetime
 
-__version__: str = "0.1.0"
 __launch__: datetime = datetime.now()
+
+
+def get_version() -> str:
+    _BASE_VERSION = "0.1.0"
+    from importlib import metadata
+
+    try:
+        package_version = metadata.version("eric")
+    except metadata.PackageNotFoundError:
+        package_version = _BASE_VERSION
+
+    from pathlib import Path
+
+    from git import Repo
+
+    if not ((git_path := Path.cwd() / ".git").exists() and git_path.is_dir()):
+        return package_version
+    if commit := next(Repo(Path.cwd()).iter_commits(), None):
+        return f"{package_version}-{commit.hexsha[:7]}"
+    return package_version
+
+
+__version__: str = get_version()
