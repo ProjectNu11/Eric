3815198752d6479c7541a81ae1852d1673a695eb
nullqwertyuiop <null@member.fsf.org>
2023-04-03 20:52:37 +0800

    :sparkles: Add corpse module check
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/util/module/launch.py
+++ library/util/module/launch.py
@@ -6,6 +6,7 @@ from loguru import logger
 
 from library.model.config import EricConfig, ModuleState, PathConfig
 from library.util.module import Modules
+from library.util.module.corpse import remove_if_corpse
 from library.util.module.get import list_module
 from library.util.module.require import require
 
@@ -14,9 +15,15 @@ def launch_require():
     config: EricConfig = create(EricConfig)
 
     create(ModuleState).initialize()
-    _lib_modules = list_module(Path("library/module"))
-    _user_modules = list_module(Path(create(PathConfig).module))
+    lib_modules = list_module(Path("library/module"))
+    user_modules = list_module(Path(create(PathConfig).module))
+
+    cleaned = [
+        module
+        for module in [*lib_modules, *user_modules]
+        if not remove_if_corpse(module)
+    ]
     modules = it(Modules)
-    modules.add(*_lib_modules, *_user_modules)
-    logger.success(f"[EricService] 已校验 {len(_lib_modules) + len(_user_modules)} 个模块")
+    modules.add(*cleaned)
+    logger.success(f"[EricService] 已校验 {len(cleaned)} 个模块")
     require(*modules.ordered, debug=config.debug)
