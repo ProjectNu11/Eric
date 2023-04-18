29bb54722a0fccabed42aaa64965c56729d8671a
nullqwertyuiop <null@member.fsf.org>
2023-04-18 23:11:32 +0800

    :sparkles: `library`: Support lock file checking
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/__init__.py
+++ library/__init__.py
@@ -1,15 +1,19 @@
+import os
+from hashlib import md5
+from importlib import metadata
+from pathlib import Path
+
+from git import Repo
+from loguru import logger
+
+
 def get_version() -> str:
     _BASE_VERSION = "0.1.0"
-    from importlib import metadata
 
     try:
         package_version = metadata.version("eric")
     except metadata.PackageNotFoundError:
         package_version = _BASE_VERSION
-
-    from pathlib import Path
-
-    from git import Repo
 
     if not ((git_path := Path.cwd() / ".git").exists() and git_path.is_dir()):
         return package_version
@@ -19,3 +23,23 @@ def get_version() -> str:
 
 
 __version__: str = get_version()
+
+
+def compare_lock():
+    lock = Path.cwd() / "pdm.lock"
+    now = md5(lock.read_text().encode()).hexdigest()
+    hashed = Path.cwd() / ".lock-hash"
+    if not hashed.exists():
+        logger.warning("未找到锁文件哈希，正在重新安装并创建")
+        os.system("pdm install")
+        hashed.write_text(now)
+        return
+    elif hashed.read_text() != now:
+        logger.warning("锁文件已被修改，正在重新安装")
+        os.system("pdm install")
+        hashed.write_text(now)
+        return
+
+
+if __name__ == "library":  # 仅在被导入时执行
+    compare_lock()
