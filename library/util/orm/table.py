de2226188f97501996432d06dc2b5c2657d08a37
nullqwertyuiop <null@member.fsf.org>
2023-04-18 23:35:36 +0800

    :construction: `library.util.orm`: Cleaned up unused tables
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/util/orm/table.py
+++ library/util/orm/table.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BIGINT, TEXT, Column, DateTime, Float, Integer, String
+from sqlalchemy import BIGINT, TEXT, Column, DateTime, Integer, String
 
 from library.util.orm import Base
 
@@ -67,27 +67,6 @@ class TempBlacklistTable(Base):
 
     duration = Column(Integer, nullable=False)
     """ 持续时间（秒） """
-
-
-class ProcessTimeStat(Base):
-    """处理时间统计"""
-
-    __tablename__ = "process_time_stat"
-
-    id = Column(Integer, primary_key=True)
-    """ 记录ID """
-
-    time = Column(DateTime, nullable=False)
-    """ 调用时间 """
-
-    module = Column(String(length=4000), nullable=False)
-    """ 模块名 """
-
-    function = Column(String(length=4000), nullable=False)
-    """ 调用功能 """
-
-    time_used = Column(Float, nullable=False)
-    """ 耗时 """
 
 
 class UserProfileTable(Base):
