08254146dae14c1af534573a5c642090914f00b1
nullqwertyuiop <null@member.fsf.org>
2023-05-17 00:05:29 +0800

    :sparkles: `library.module.usage_report`: Add coverage report
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- library/module/usage_report/__init__.py
+++ library/module/usage_report/__init__.py
@@ -1,11 +1,14 @@
 from graia.ariadne import Ariadne
 from graia.ariadne.event.message import FriendMessage, GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Image
 from graia.ariadne.message.parser.twilight import FullMatch, Twilight
 from graia.saya import Channel
 from graiax.shortcut import decorate, dispatch, listen
 
 from library.decorator import Blacklist, Distribution, FunctionCall, Switch
-from library.module.usage_report.util import get_report
+from library.module.usage_report.coverage import get_page
+from library.module.usage_report.stats import get_report
 from library.util.dispatcher import PrefixMatch
 from library.util.message import send_message
 from library.util.typ import Message
@@ -29,3 +32,24 @@ channel = Channel.current()
 )
 async def get_usage_report(app: Ariadne, event: Message):
     await send_message(event, await get_report(app, event.sender), app.account)
+
+
+@listen(GroupMessage, FriendMessage)
+@dispatch(
+    Twilight(
+        PrefixMatch(),
+        FullMatch("coverage"),
+    )
+)
+@decorate(
+    Switch.check(channel.module),
+    Distribution.distribute(),
+    Blacklist.check(),
+    FunctionCall.record(channel.module),
+)
+async def get_coverrge_report(app: Ariadne, event: Message):
+    await send_message(
+        event,
+        MessageChain(Image(data_bytes=await (await get_page()).render())),
+        app.account,
+    )
---
+++ library/module/usage_report/coverage.py
@@ -0,0 +1,76 @@
+from creart import it
+from graia.ariadne import Ariadne
+from graia.ariadne.model import Friend, Group, Member
+
+from library.ui import Banner, Page, ProgressBar
+from library.util.multi_account.public_group import PublicGroup
+
+
+async def get_all_members() -> list[Member]:
+    public_group: PublicGroup = it(PublicGroup)
+    result: list[Member] = []
+    for account in public_group.accounts:
+        for group in Ariadne.current(account).get_group_list():
+            group: Group
+            result.extend(await Ariadne.current(account).get_member_list(group))
+    return result
+
+
+async def get_all_friends() -> list[Friend]:
+    public_group: PublicGroup = it(PublicGroup)
+    result: list[Friend] = []
+    for account in public_group.accounts:
+        result.extend(await Ariadne.current(account).get_friend_list())
+    return result
+
+
+def deduplicate(data: list[Member | Friend]) -> set[int]:
+    return {int(this) for this in data}
+
+
+async def get_dedup_members() -> set[int]:
+    return deduplicate(await get_all_members())
+
+
+async def get_dedup_friends() -> set[int]:
+    return deduplicate(await get_all_friends())
+
+
+async def get_all_users() -> list[Member | Friend]:
+    members: list[Member] = await get_all_members()
+    friends: list[Friend] = await get_all_friends()
+    return [*members, *friends]
+
+
+async def get_dedup_all() -> set[int]:
+    members: set[int] = await get_dedup_members()
+    friends: set[int] = await get_dedup_friends()
+    return members | friends
+
+
+async def get_page():
+    all_members = await get_all_members()
+    dedup_members = await get_dedup_members()
+    all_friends = await get_all_friends()
+    dedup_friends = await get_dedup_friends()
+    all_users = await get_all_users()
+    dedup_users = await get_dedup_all()
+
+    return Page(
+        Banner("用户覆盖率统计"),
+        ProgressBar(
+            len(dedup_members) / len(all_members),
+            "群员",
+            f"{len(dedup_members)} 去重复 / {len(all_members)} 总数",
+        ),
+        ProgressBar(
+            len(dedup_friends) / len(all_friends),
+            "好友",
+            f"{len(dedup_friends)} 去重复 / {len(all_friends)} 总数",
+        ),
+        ProgressBar(
+            len(dedup_users) / len(all_users),
+            "总数",
+            f"{len(dedup_users)} 去重复 / {len(all_users)} 总数",
+        ),
+    )
